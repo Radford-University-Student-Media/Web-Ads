b4d46c8c0711221f3ac06f87bd9b4df0d1c3ed62
Corey Roadcap <croadcap@radford.edu>
2012-04-18 12:22:16 -0400

    Changing create ad layout

--- lib/Site/CreateAdBody.php
+++ lib/Site/CreateAdBody.php
@@ -52,10 +52,11 @@ class CreateAdBody extends Body{
 			<input type=\"text\" name=\"url\" placeholder=\"Click Url\" class=\"bluefocus text\" tabindex=\"2\"\>
 			<label for=\"size\" class=\"above\">Size</label>
 			".Size::getSelectHTHL("size", 5)."<br /><br />
-			<input type=\"submit\" value=\"Create\" class=\"stdbutton bluefocus\" tabindex=\"6\"/>
+			
+		<input type=\"file\" name=\"image\" id=\"image\" style=\"\" tabindex=\"6\"/>
+			<input type=\"submit\" value=\"Create\" class=\"stdbutton bluefocus\" tabindex=\"7\"/>
 		</div>
 		<input type=\"hidden\" name=\"action\" value=\"createAd\" />
-		<input type=\"file\" name=\"image\" id=\"image\" style=\"\" tabindex=\"6\"/>
 		</form>
 		
 		</div>"; 
